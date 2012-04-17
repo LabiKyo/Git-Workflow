05714d17ae9cf8f7a8175050bccabeec64a03254
Labi Kyo <labikyo@gmail.com>
2012-04-18 05:08:32 +0800

    initialize slideshow

---
+++ js/main.js
@@ -0,0 +1,10 @@
+Reveal.initialize({
+  controls: false,
+  progress: true,
+  history: true,
+  mouseWheel: true,
+  rollingLinks: true,
+  theme: 'neon', // default/neon
+  transition: 'concave' // default/cube/page/concave/linear(2d)
+});
+
